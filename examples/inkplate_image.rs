04a3dc4d647a807166ed385078b9f8c5f50e7939
Tommy Gilligan <7865781+tommy-gilligan@users.noreply.github.com>
2024-01-27 22:23:15 +1100

    use custom fn for dither in image example

--- examples/inkplate_image.rs
+++ examples/inkplate_image.rs
@@ -9,6 +9,7 @@
 #![doc = ::embed_doc_image::embed_image!("photo", "examples/image_photo.jpg")]
 //!
 
+use ab1024_ega::color::Color;
 use dither::DitherTarget;
 use embedded_graphics::{pixelcolor::Rgb888, prelude::*};
 use embedded_hal_bus::spi::ExclusiveDevice;
@@ -22,6 +23,42 @@ use hal::{
     Delay, Rtc,
 };
 use tinybmp::Bmp;
+
+const RGB_DISPLAY_PAIRS: [(Rgb888, Color); 7] = [
+    (Rgb888::new(0x00, 0x00, 0x00), Color::BLACK),
+    (Rgb888::new(0xFF, 0xFF, 0xFF), Color::WHITE),
+    (Rgb888::new(0x10, 0xcb, 0x10), Color::GREEN),
+    (Rgb888::new(0x20, 0x20, 0xff), Color::BLUE),
+    (Rgb888::new(0xff, 0x30, 0x20), Color::RED),
+    (Rgb888::new(0xff, 0xff, 0x50), Color::YELLOW),
+    (Rgb888::new(0xf0, 0x70, 0x20), Color::ORANGE),
+];
+
+fn rgb_to_epd(color: Rgb888) -> Color {
+    let (_, display) = RGB_DISPLAY_PAIRS
+        .into_iter()
+        .min_by_key(|(rgb, _): &(Rgb888, Color)| {
+            let r: u16 =
+                (<u8 as Into<u16>>::into(color.r())).abs_diff(<u8 as Into<u16>>::into(rgb.r()));
+            let g: u16 =
+                (<u8 as Into<u16>>::into(color.g())).abs_diff(<u8 as Into<u16>>::into(rgb.g()));
+            let b: u16 =
+                (<u8 as Into<u16>>::into(color.b())).abs_diff(<u8 as Into<u16>>::into(rgb.b()));
+            r + g + b
+        })
+        .unwrap();
+
+    display
+}
+
+fn epd_to_rgb(color: Color) -> Rgb888 {
+    let (display, _) = RGB_DISPLAY_PAIRS
+        .into_iter()
+        .find(|(_, c): &(Rgb888, Color)| *c == color)
+        .unwrap();
+
+    display
+}
 
 #[entry]
 fn main() -> ! {
@@ -46,11 +83,8 @@ fn main() -> ! {
     let bmp: Bmp<Rgb888> = Bmp::from_slice(include_bytes!("starry-night.bmp")).unwrap();
     let mut display = ab1024_ega::Display::new(spi, rst, dc, busy, delay);
 
-    let mut ed: DitherTarget<'_, _, _, _, { ab1024_ega::WIDTH + 1 }> = DitherTarget::new(
-        &mut display,
-        &|x: Rgb888| x.into(),
-        &|y: ab1024_ega::color::Color| y.into(),
-    );
+    let mut ed: DitherTarget<'_, _, _, _, { ab1024_ega::WIDTH + 1 }> =
+        DitherTarget::new(&mut display, &rgb_to_epd, &epd_to_rgb);
     bmp.draw(&mut ed).unwrap();
 
     display.init().unwrap();
